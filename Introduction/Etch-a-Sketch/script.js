4ee2ff2a96678a6142822ff0d4b885f214c5ba66
Zane Richards <zanerosu@gmail.com>
2024-03-04 22:19:30 -0500

    Added painting and resizeable grid

--- Introduction/Etch-a-Sketch/script.js
+++ Introduction/Etch-a-Sketch/script.js
@@ -1,9 +1,36 @@
 mainDiv = document.querySelector('#mainContainer');
 
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
-        const gridDiv = document.createElement('div');
-        gridDiv.setAttribute('class', 'gridDiv');
-        mainDiv.appendChild(gridDiv);
+//Creates grid based on row and col parameter
+function generateGrid(userSize){
+    for (let i = 0; i < userSize; i++){
+        for (let j = 0; j < userSize; j++){
+            const gridDiv = document.createElement('div');
+            gridDiv.setAttribute('class', 'gridDiv');
+            gridDiv.style.width = `${480/userSize}px`;
+            gridDiv.style.height = `${480/userSize}px`;
+            mainDiv.appendChild(gridDiv);
+            
+            gridDiv.addEventListener('mouseenter', (event) => {
+                gridDiv.style.backgroundColor = 'rgb(49, 36, 36)'
+            })
+        }
     }
 }
+
+//Generates Initial Grid
+generateGrid(16)
+
+
+//Clears current grid on screen 
+function removeGrid(){
+    mainDiv.innerHTML = '';
+}   
+
+
+
+btnNewGrid = document.querySelector('#newGrid');
+btnNewGrid.addEventListener('click', ()=>{
+    let userSize = prompt("Enter grid size (Max 100): ")
+    removeGrid()
+    generateGrid(userSize);
+})
