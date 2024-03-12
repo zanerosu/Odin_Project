c8bb7767403ba35b5dabe84ac390ffc2e8087686
Zane Richards <zanerosu@gmail.com>
2024-03-12 00:41:00 -0400

    Added button functionality for numbers and clear buttons

--- Introduction/Calculator/script.js
+++ Introduction/Calculator/script.js
@@ -14,6 +14,7 @@ function divide(a, b){
     return a / b
 }
 
+
 const numA = ""
 const numB = ""
 const operator = ""
@@ -36,3 +37,30 @@ function operate(numA, numB, operator){
         alert('Invalid operator!')
     }
 }
+
+const display = document.querySelector("#display");
+const num_btns = document.querySelector("#num-btns");
+const numberRegex = /^[0-9]$/;
+
+//Adds number to display if the id of the button clicked is a value of 0-9;
+num_btns.addEventListener('click', (event) =>{
+    if(numberRegex.test(event.target.id)){
+        display.textContent += event.target.id
+    }
+})
+
+const clearAll = document.querySelector("#clearAll");
+const clear = document.querySelector("#clear");
+
+//Clears entire display
+clearAll.addEventListener('click', () =>{
+    display.textContent = "";
+})
+
+//Clears last value in display
+clear.addEventListener('click', (event) =>{
+    display.textContent = display.textContent.slice(0, -1);
+})
+
+
+
