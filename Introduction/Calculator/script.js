149b35166237a2b3e8d64bf0ba51b96821511707
Zane Richards <zanerosu@gmail.com>
2024-03-14 21:02:25 -0400

    Completed final logic and added more styling for clearer calculations.

--- Introduction/Calculator/script.js
+++ Introduction/Calculator/script.js
@@ -37,77 +37,12 @@ function operate(numA, numB, operator){
     }
 };
 
-const display = document.querySelector("#display");
-display.textContent = '0'
-
-//--------------Number button and operator button functions---------------------
-const num_btns = document.querySelector("#num-btns");
-const numberRegex = /^[0-9]$/;
-const operator_btns = document.querySelector("#operator-btns");
-
-//Adds number to display if the id of the button clicked is a value of 0-9;
-num_btns.addEventListener('click', (event) =>{
-    if(numberRegex.test(event.target.id)){
-        if (display.textContent === '0'){
-            display.textContent = event.target.id;
-        } else{
-            display.textContent += event.target.id;
-        }
-    }
-    console.log("NUM CLICK")
-    console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
-});
-
-//Stores first number and operation into respective variables
-operator_btns.addEventListener('click', (event) =>{
-    if (numA != "" && Number(total) != Number(display.textContent)){
-        numB = Number(display.textContent)
-        total = String(operate(numA, numB, operator));
-        operator = event.target.id;
-        display.textContent = total;
-        numA = Number(total);
-
-        hasDecimal = false;
-        resultArray = display.textContent.split("")
-        resultArray.forEach(element => {
-        if (element === '.'){
-            hasDecimal = true;
-        }
-    });
-
-    }else{
-        numA = Number(display.textContent);
-        operator = event.target.id;
-        display.textContent = "";
-    }
-    console.log("OPERATOR CLICK")
-    console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
-});
-
-//-------------------Equal button function----------------------------
-const equalBtn = document.querySelector("#equal");
-
-equalBtn.addEventListener('click', ()=>{
+function performOperation(){
     numB = Number(display.textContent);
     total = String(operate(numA, numB, operator));
-    display.textContent = total;
-
-    hasDecimal = false;
-    resultArray = display.textContent.split("")
-    resultArray.forEach(element => {
-        if (element === '.'){
-            hasDecimal = true;
-        }
-    });
-
-    console.log("EQUAL CLICK")
-    console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
-})
-
-
-//-------------------Clear button functions--------------------------
-const clearAll = document.querySelector("#clearAll");
-const clear = document.querySelector("#clear");
+    updateDisplay(total);
+    numA = Number(total);
+}
 
 function resetAll(){
     display.textContent = "0";
@@ -116,7 +51,89 @@ function resetAll(){
     total = "";
     operator = "";
     hasDecimal = false;
+    if (lastOp){
+        lastOp.classList.remove('op-button-current');
+    }
 }
+
+function updateDisplay(content){
+    const maxLength = 13;
+    if (content.length > maxLength){
+        display.textContent = content.slice(0, maxLength)
+    } else {
+        display.textContent = content;
+    }
+}
+
+
+const display = document.querySelector("#display");
+display.textContent = '0'
+
+//--------------Number button and operator button functions---------------------
+const num_btns = document.querySelector("#num-btns");
+const numberRegex = /^[0-9]$/;
+
+
+//Adds number to display if the id of the button clicked is a value of 0-9;
+num_btns.addEventListener('click', (event) =>{
+    if(numberRegex.test(event.target.id)){
+        if (display.textContent === '0' || display.textContent === total){
+            updateDisplay(event.target.id);
+        } else{
+            updateDisplay(display.textContent + event.target.id);
+        }
+    }
+    console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
+});
+
+//Stores first number and operation into respective variables
+let lastOp = null;
+const operator_btns = document.querySelectorAll('.op-button'); 
+operator_btns.forEach(button =>{
+    button.addEventListener('click', (event) => {
+        if (lastOp){
+            lastOp.classList.remove('op-button-current');
+        }
+        if (numA !== ""){
+            performOperation();
+        } else {
+            numA = Number(display.textContent);
+        }
+        operator = event.target.id;
+        if(numA === Number(total)){
+            display.textContent = total
+        } else {
+            display.textContent = "";
+        }
+        lastOp = event.target;
+        event.target.classList.add('op-button-current');
+        console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
+    })
+});
+
+//-------------------Equal button function----------------------------
+const equalBtn = document.querySelector("#equal");
+
+equalBtn.addEventListener('click', ()=>{
+    if (lastOp){
+        lastOp.classList.remove('op-button-current');
+    }
+    if (numA !== "" && operator !== ""){
+        if (numB === ""){
+            numB = Number(display.textContent)
+        }
+        performOperation();
+        numA = ""
+        numB = ""
+        operator = ""
+    }
+    console.log(`Operation = ${operator} numA = ${numA} numB = ${numB} total = ${total}`);
+})
+
+
+//-------------------Clear button functions--------------------------
+const clearAll = document.querySelector("#clearAll");
+const clear = document.querySelector("#clear");
 
 //Clears entire display
 clearAll.addEventListener('click', () =>{
