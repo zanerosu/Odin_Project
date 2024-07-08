8804ca0ea5170657777f9f72202cfbe53127641b
zanerosu <zanerosu@gmail.com>
2024-07-08 00:34:28 -0400

    Created directory for React course and created initial react app.

---
+++ React/my-first-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Hello World!
+      </p>
+    </>
+  )
+}
+
+export default App
